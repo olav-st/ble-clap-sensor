3d435c2009027c73d662b40159ea92bffa6392a1
olav-st <olav.s.th@gmail.com>
2021-03-09 09:21:29 +0100

    Fix indentation

--- src/clap_detection.c
+++ src/clap_detection.c
@@ -22,12 +22,12 @@ const struct device *adc_dev;
 #define ADC_1ST_CHANNEL_INPUT NRF_SAADC_INPUT_AIN7
 
 static const struct adc_channel_cfg m_1st_channel_cfg = {
-	.gain = ADC_GAIN,
-	.reference = ADC_REFERENCE,
-	.acquisition_time = ADC_ACQUISITION_TIME,
-	.channel_id = ADC_1ST_CHANNEL_ID,
+    .gain = ADC_GAIN,
+    .reference = ADC_REFERENCE,
+    .acquisition_time = ADC_ACQUISITION_TIME,
+    .channel_id = ADC_1ST_CHANNEL_ID,
 #if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
-	.input_positive = ADC_1ST_CHANNEL_INPUT,
+    .input_positive = ADC_1ST_CHANNEL_INPUT,
 #endif
 };
 
@@ -50,19 +50,19 @@ static int clap_detection_settings_set(const char *name, size_t len, settings_re
     int rc;
 
     if (settings_name_steq(name, "treshold", &next) && !next)
-	{
+    {
         if (len != sizeof(treshold))
-		{
+        {
             return -EINVAL;
         }
 
         rc = read_cb(cb_arg, &treshold, sizeof(treshold));
         if (rc >= 0)
-		{
+        {
             return 0;
         }
 
-		printk("Loded new treshold value: %d", treshold);
+        printk("Loded new treshold value: %d", treshold);
 
         return rc;
     }
@@ -79,122 +79,122 @@ struct settings_handler settings_conf =
 
 int adc_init()
 {
-	int err;
+    int err;
 
-	adc_dev = device_get_binding(DT_LABEL(DT_NODELABEL(adc)));
-	if (!adc_dev)
-	{
-		LOG_ERR("device_get_binding %s failed\n", DT_LABEL(DT_NODELABEL(adc)));
-	}
-	err = adc_channel_setup(adc_dev, &m_1st_channel_cfg);
-	if (err)
-	{
-		LOG_ERR("Error in adc setup: %d\n", err);
-	}
+    adc_dev = device_get_binding(DT_LABEL(DT_NODELABEL(adc)));
+    if (!adc_dev)
+    {
+        LOG_ERR("device_get_binding %s failed\n", DT_LABEL(DT_NODELABEL(adc)));
+    }
+    err = adc_channel_setup(adc_dev, &m_1st_channel_cfg);
+    if (err)
+    {
+        LOG_ERR("Error in adc setup: %d\n", err);
+    }
 
-	return err;
+    return err;
 }
 
 int adc_sample(int16_t* buffer, size_t num_samples)
 {
-	int ret;
+    int ret;
 
-	const struct adc_sequence_options options =
-	{
-		.extra_samplings = num_samples - 1,
-		.interval_us     = 0,
-	};
+    const struct adc_sequence_options options =
+    {
+        .extra_samplings = num_samples - 1,
+        .interval_us     = 0,
+    };
 
-	const struct adc_sequence sequence =
-	{
-		.channels = BIT(ADC_1ST_CHANNEL_ID),
-		.buffer = buffer,
-		.buffer_size = sizeof(buffer) * num_samples,
-		.resolution = ADC_RESOLUTION,
-		.options = &options,
-	};
+    const struct adc_sequence sequence =
+    {
+        .channels = BIT(ADC_1ST_CHANNEL_ID),
+        .buffer = buffer,
+        .buffer_size = sizeof(buffer) * num_samples,
+        .resolution = ADC_RESOLUTION,
+        .options = &options,
+    };
 
-	if (!adc_dev)
-	{
-		return -1;
-	}
+    if (!adc_dev)
+    {
+        return -1;
+    }
 
-	ret = adc_read(adc_dev, &sequence);
-	if(ret < 0)
-	{
-		LOG_ERR("ADC read err: %d\n", ret);
-	}
+    ret = adc_read(adc_dev, &sequence);
+    if(ret < 0)
+    {
+        LOG_ERR("ADC read err: %d\n", ret);
+    }
 
-	return ret;
+    return ret;
 }
 
 int clap_detection_init()
 {
-	int err = adc_init();
-	if(err)
-	{
-		return err;
-	}
-	settings_subsys_init();
-	settings_register(&settings_conf);
-	settings_load();
-	LOG_INF("Loaded treshold value: %d", treshold);
+    int err = adc_init();
+    if(err)
+    {
+        return err;
+    }
+    settings_subsys_init();
+    settings_register(&settings_conf);
+    settings_load();
+    LOG_INF("Loaded treshold value: %d", treshold);
 
-	return 0;
+    return 0;
 }
 
 bool clap_detection_check()
 {
-	int16_t sample;
-	int err = adc_sample(&sample, 1);
-	if(err)
-	{
-		LOG_ERR("Failed to sample using ADC: %d\n", err);
-		return false;
-	}
-	//Add the sample to the ring buffer
-	ring_buf_put(&long_term_sample_buffer, &sample, sizeof(sample));
-	ring_buf_put(&short_term_sample_buffer, &sample, sizeof(sample));
+    int16_t sample;
+    int err = adc_sample(&sample, 1);
+    if(err)
+    {
+        LOG_ERR("Failed to sample using ADC: %d\n", err);
+        return false;
+    }
+    //Add the sample to the ring buffer
+    ring_buf_put(&long_term_sample_buffer, &sample, sizeof(sample));
+    ring_buf_put(&short_term_sample_buffer, &sample, sizeof(sample));
 
-	if(ring_buf_space_get(&long_term_sample_buffer) == 0 &&
-		ring_buf_space_get(&short_term_sample_buffer) == 0
-		)
-	{
-		//Calculate long term average
-		long_term_average = 0;
-		for (int i = 0; i < NUM_SAMPLES_LONG_TERM_AVG; i++) 
-		{
-			ring_buf_get(&long_term_sample_buffer, &sample, sizeof(sample));
-			long_term_average += abs(sample);
-		}
-		long_term_average = long_term_average / NUM_SAMPLES_LONG_TERM_AVG;
+    if(ring_buf_space_get(&long_term_sample_buffer) == 0 &&
+        ring_buf_space_get(&short_term_sample_buffer) == 0
+        )
+    {
+        //Calculate long term average
+        long_term_average = 0;
+        for (int i = 0; i < NUM_SAMPLES_LONG_TERM_AVG; i++) 
+        {
+            ring_buf_get(&long_term_sample_buffer, &sample, sizeof(sample));
+            long_term_average += abs(sample);
+        }
+        long_term_average = long_term_average / NUM_SAMPLES_LONG_TERM_AVG;
 
-		//Calculate short term average
-		short_term_average = 0;
-		for (int i = 0; i < NUM_SAMPLES_SHORT_TERM_AVG; i++) 
-		{
-			ring_buf_get(&short_term_sample_buffer, &sample, sizeof(sample));
-			short_term_average += abs(sample);
-		}
-		short_term_average = short_term_average / NUM_SAMPLES_SHORT_TERM_AVG;
+        //Calculate short term average
+        short_term_average = 0;
+        for (int i = 0; i < NUM_SAMPLES_SHORT_TERM_AVG; i++) 
+        {
+            ring_buf_get(&short_term_sample_buffer, &sample, sizeof(sample));
+            short_term_average += abs(sample);
+        }
+        short_term_average = short_term_average / NUM_SAMPLES_SHORT_TERM_AVG;
 
-		//Check for a clap
-		int clap_likeness = abs(short_term_average-long_term_average);
-		//printk("C: %d, T: %d\n", clap_likeness, treshold);
-		//printk("ST: %d, LT: %d\n", short_term_average, long_term_average);
-		if(clap_likeness > treshold)
-		{
-			ring_buf_reset(&short_term_sample_buffer);
-			k_msleep(50);
-			return true;
-		}
-	}
-	return false;
+        //Check for a clap
+        int clap_likeness = abs(short_term_average-long_term_average);
+        //printk("C: %d, T: %d\n", clap_likeness, treshold);
+        //printk("ST: %d, LT: %d\n", short_term_average, long_term_average);
+        if(clap_likeness > treshold)
+        {
+            ring_buf_reset(&short_term_sample_buffer);
+            k_msleep(50);
+            return true;
+        }
+    }
+    return false;
 }
 
 void clap_detection_set_treshold(int value)
 {
-	LOG_INF("Saving new treshold value: %d", value);
-	settings_save_one("clap_detection/treshold", &value, sizeof(value));
-	settings_load();
+    LOG_INF("Saving new treshold value: %d", value);
+    settings_save_one("clap_detection/treshold", &value, sizeof(value));
+    settings_load();
 }
--- src/main.c
+++ src/main.c
@@ -11,63 +11,63 @@ static int num_claps = 0;
 // Timer that resets num_claps after 1 second
 void reset_num_claps(struct k_timer *timer)
 {
-	num_claps = 0;
+    num_claps = 0;
 }
 K_TIMER_DEFINE(reset_claps_timer, reset_num_claps, NULL);
 
 int main(void)
 {
-	int err;
-	
-	err = led_init();
-	if (err)
-	{
-		LOG_ERR("Failed to initalize LED (err %d)", err);
-	}
+    int err;
+    
+    err = led_init();
+    if (err)
+    {
+        LOG_ERR("Failed to initalize LED (err %d)", err);
+    }
 
-	err = clap_detection_init();
-	if (err)
-	{
-		LOG_ERR("Failed to initalize clap detection (err %d)", err);
-	}
+    err = clap_detection_init();
+    if (err)
+    {
+        LOG_ERR("Failed to initalize clap detection (err %d)", err);
+    }
 
-	err = ble_services_init();
-	if (err)
-	{
-		LOG_ERR("Failed to initialize BLE button service (err %d)", err);
-	}
+    err = ble_services_init();
+    if (err)
+    {
+        LOG_ERR("Failed to initialize BLE button service (err %d)", err);
+    }
 
-	// Show a recognizable LED pattern on startup
-	led_set_state(true);
-	k_msleep(500);
-	led_set_state(false);
-	k_msleep(500);
-	led_set_state(true);
-	k_msleep(500);
-	led_set_state(false);
-	
-	// Start listening for claps
-	while (1) 
-	{
-		if(clap_detection_check())
-		{
-			LOG_INF("Clap!");
-			num_claps++;
-			
-			k_timer_start(&reset_claps_timer, K_SECONDS(1), K_SECONDS(1));
-		}
+    // Show a recognizable LED pattern on startup
+    led_set_state(true);
+    k_msleep(500);
+    led_set_state(false);
+    k_msleep(500);
+    led_set_state(true);
+    k_msleep(500);
+    led_set_state(false);
+    
+    // Start listening for claps
+    while (1) 
+    {
+        if(clap_detection_check())
+        {
+            LOG_INF("Clap!");
+            num_claps++;
+            
+            k_timer_start(&reset_claps_timer, K_SECONDS(1), K_SECONDS(1));
+        }
 
-		if(num_claps == 2)
-		{
-			num_claps = 0;
+        if(num_claps == 2)
+        {
+            num_claps = 0;
 
-			ble_services_set_button_state(true);
-			led_set_state(true);
+            ble_services_set_button_state(true);
+            led_set_state(true);
 
-			k_msleep(1000);
+            k_msleep(1000);
 
-			ble_services_set_button_state(false);
-			led_set_state(false);
-		}
-	}
+            ble_services_set_button_state(false);
+            led_set_state(false);
+        }
+    }
 }
